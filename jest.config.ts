1b6d02c551ad95c8943439876a5b730580477a45
Reya Seda <reyas.tech@gmail.com>
2025-03-07 15:57:39 -0700

    more jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  testEnvironment: "node",
+  preset: 'ts-jest',
   transform: {
-    "^.+\.tsx?$": ["ts-jest",{}],
-  },
+    '^.+\\.(ts|tsx)?$': 'ts-jest'
+  }
 };
