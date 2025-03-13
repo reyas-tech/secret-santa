56a44101b284c4b783493bb848698f790c2d70f6
Reya Seda <reyas.tech@gmail.com>
2025-03-13 16:15:02 -0600

    update questionnaire validation and unit testing

--- src/app/questionnaire/questionnaire.component.spec.ts
+++ src/app/questionnaire/questionnaire.component.spec.ts
@@ -1,44 +1,65 @@
 import { QuestionnaireComponent } from './questionnaire.component';
 import { render, RenderResult } from '@testing-library/angular';
-import userEvent from '@testing-library/user-event';
+import userEvent, { UserEvent } from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { waitFor } from '@testing-library/dom';
 
 let component: RenderResult<QuestionnaireComponent>;
+let user: UserEvent;
 
-const initComponent = async () => ({
-  user: userEvent.setup(),
-  ...render(QuestionnaireComponent, {
-    imports: [
-      CommonModule,
-      FormsModule,
-      ReactiveFormsModule
-    ],
-    declarations: [],
-    providers: []
-  })
-});
-
-beforeEach(async () => {
-  component = await initComponent();
-})
-
-describe('Given the user opens the questionnaire', () => {
-  it('should display all questions with proper accessibility', () => {
-    const questions = component.fixture.componentInstance.questions;
-    let input: HTMLElement;
-    questions.forEach(q => {
-      input = component.getByLabelText(q);
-      expect(input).toBeVisible();
-    })
-  });
-
-  describe('when the user fills out the questionnaire', () => {
-    it('should', () => {});
-
-    describe('when the user submits the questionnaire', () => {
-      it('should', () => {});
+const initComponent = async () => {
+    user = userEvent.setup();
+    return render(QuestionnaireComponent, {
+        imports: [
+            CommonModule,
+            FormsModule,
+            ReactiveFormsModule,
+            MatFormFieldModule,
+            MatInputModule,
+        ],
+        declarations: [],
+        providers: [],
     });
-  });
+};
+
+describe('given user opens questionnaire', () => {
+    it('should allow user to submit and reset questionnaire once all questions are answered', async () => {
+        component = await initComponent();
+
+        const submit = component.getByRole('button', { name: 'Submit' });
+        expect(submit).toHaveAccessibleName();
+        
+        let input: HTMLElement;
+        component.fixture.componentInstance.questions.forEach(
+            async (question) => {
+                // Submit button should be disabled until all questions are answered
+                expect(submit).toBeDisabled();
+                // Each question should have a corresponding form input
+                input = component.getByLabelText(question);
+                expect(input).toBeVisible();
+                expect(input).toHaveAccessibleName();
+                await user.type(input, 'test');
+                expect(input).toHaveValue('test');
+            }
+        );
+
+        waitFor(() => {
+            // Submit button should be enabled once all questions are answered
+            expect(submit).toBeEnabled();
+        });
+
+        // On submit, should disable submit button and reset form
+        await user.click(submit);
+        expect(submit).toBeDisabled();
+        component.fixture.componentInstance.questions.forEach(
+            async (question) => {
+                input = component.getByLabelText(question);
+                expect(input).toHaveValue('');
+            }
+        );
+    });
 });
--- src/app/questionnaire/questionnaire.component.ts
+++ src/app/questionnaire/questionnaire.component.ts
@@ -1,25 +1,32 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-questionnaire',
-  standalone: false,
-  templateUrl: './questionnaire.component.html',
-  styleUrl: './questionnaire.component.scss',
+    selector: 'app-questionnaire',
+    standalone: false,
+    templateUrl: './questionnaire.component.html',
+    styleUrl: './questionnaire.component.scss',
 })
 export class QuestionnaireComponent {
-  questions = [
-    'Question 1',
-    'Question 2',
-    'Question 3',
-    'Question 4',
-    'Question 5'
-  ];
-  questionnaire = new FormGroup({});
-  
-  constructor() {
-    this.questions.forEach((q, i) => {
-      this.questionnaire.addControl(i.toString(), new FormControl<string | undefined>(undefined));
-    })
-  }
+    questions = [
+        'Question 1',
+        'Question 2',
+        'Question 3',
+        'Question 4',
+        'Question 5',
+    ];
+    questionnaire = new FormGroup({});
+
+    constructor() {
+        this.questions.forEach((q, i) => {
+            this.questionnaire.addControl(
+                i.toString(),
+                new FormControl<string>('', Validators.required)
+            );
+        });
+    }
+
+    onSubmit() {
+        this.questionnaire.reset();
+    }
 }
