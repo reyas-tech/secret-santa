c7672bc4706dbfc3206e72663595c7c6e64896e3
Reya Seda <reyas.tech@gmail.com>
2025-03-14 09:46:42 -0600

    add nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,19 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { QuestionnaireComponent } from './questionnaire/questionnaire.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
-const materialImports = [MatFormFieldModule, MatInputModule, MatButtonModule];
+const materialImports = [MatFormFieldModule, MatInputModule, MatButtonModule, MatToolbarModule];
 
 @NgModule({
-    declarations: [AppComponent, LoginComponent, QuestionnaireComponent, PageNotFoundComponent],
+    declarations: [AppComponent, LoginComponent, QuestionnaireComponent, PageNotFoundComponent, NavBarComponent],
     imports: [
         BrowserModule,
         AppRoutingModule,
